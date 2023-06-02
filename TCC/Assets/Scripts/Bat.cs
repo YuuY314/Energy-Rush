7ab554d1a1f2f29b79319f9ca0d969a7bf343f73
YuuY314 <rafaelyu0202@gmail.com>
2023-06-02 16:13:17 -0300

    Add Slime and Bat

--- TCC/Assets/Scripts/Bat.cs
+++ TCC/Assets/Scripts/Bat.cs
@@ -5,21 +5,35 @@ using Pathfinding;
 
 public class Bat : MonoBehaviour
 {
-    public AIPath aiPath;
+    public float enemySpeedX;
+    public float enemySpeedY;
     private bool isFacingRight = true;
+
+    public CircleCollider2D cc;
+    public Rigidbody2D rb;
 
     void Update()
     {
-        if(aiPath.desiredVelocity.x >= 0.01f && !isFacingRight){
-            Flip();
-        } else if(aiPath.desiredVelocity.x <= -0.01f && isFacingRight){
-            Flip();
-        }
+        Move();
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(enemySpeedX, enemySpeedY);
     }
 
     void Flip()
     {
         isFacingRight = !isFacingRight;
         transform.Rotate(0, 180, 0);
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag != "Trigger" && collider.gameObject.tag != "Collectable"){
+            enemySpeedX = -enemySpeedX;
+            enemySpeedY = -enemySpeedY;
+            Flip();
+        }
     }
 }
